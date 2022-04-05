282bd760a240a260ec8109460057a8743ff423be
ppabianczyk <pawel.pabianczyk1999@gmail.com>
2022-04-05 13:41:39 +0200

    add basic config

---
+++ ztbd-oracle/src/main/java/pl/pk/ztbdoracle/ZtbdOracleApplication.java
@@ -0,0 +1,12 @@
+package pl.pk.ztbdoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtbdOracleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZtbdOracleApplication.class, args);
+  }
+}
