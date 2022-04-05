c47044666cfe8b52d1854e89c0b1e2b3cdc22224
ppabianczyk <pawel.pabianczyk1999@gmail.com>
2022-04-05 12:32:36 +0200

    add basic config

---
+++ ztbd-postgresql/src/main/java/pl/pk/ztbdpostgresql/ZtbdPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package pl.pk.ztbdpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtbdPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtbdPostgresqlApplication.class, args);
+    }
+
+}
