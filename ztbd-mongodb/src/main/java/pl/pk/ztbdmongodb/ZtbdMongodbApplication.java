7c02ee37a837e10f7a487b8e1cf3f76fd08704aa
ppabianczyk <pawel.pabianczyk1999@gmail.com>
2022-04-05 18:20:09 +0200

    add basic config

---
+++ ztbd-mongodb/src/main/java/pl/pk/ztbdmongodb/ZtbdMongodbApplication.java
@@ -0,0 +1,13 @@
+package pl.pk.ztbdmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtbdMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtbdMongodbApplication.class, args);
+    }
+
+}
