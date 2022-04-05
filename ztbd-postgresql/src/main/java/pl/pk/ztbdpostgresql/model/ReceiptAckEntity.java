40e16c6ca62792c61eddc0dee692ee379617458e
ppabianczyk <pawel.pabianczyk1999@gmail.com>
2022-04-05 12:33:16 +0200

    add models

---
+++ ztbd-postgresql/src/main/java/pl/pk/ztbdpostgresql/model/ReceiptAckEntity.java
@@ -0,0 +1,24 @@
+package pl.pk.ztbdpostgresql.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "potwierdzenie_odbioru")
+public class ReceiptAckEntity {
+    @Id
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_id_przesylki", referencedColumnName = "id")
+    private ParcelEntity parcel;
+
+    @OneToOne(mappedBy = "receiptAck")
+    private NotificationEntity notification;
+
+    @Column(name = "data_dostarczenia")
+    private LocalDate deliveryDate;
+
+    @Column(name = "czy_dostarczono")
+    private Boolean isDelivered;
+}
