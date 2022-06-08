0db4a21693d354439d8928627673e96815a868f8
ppabianczyk <pawel.pabianczyk1999@gmail.com>
2022-06-08 17:39:37 +0200

    remove unnecessary code

--- ztbd-relational/src/main/java/pl/pk/ztbdrelational/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.pk.ztbdrelational.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.pk.ztbdrelational.model.AddressEntity;
-
-public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
-}
