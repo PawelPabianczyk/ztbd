65f73a2bf4d05aed8b332bce3eb5e29801757896
ppabianczyk <pawel.pabianczyk1999@gmail.com>
2022-06-12 15:11:21 +0200

    replace class with record

--- ztbd-relational/src/main/java/pl/pk/ztbdrelational/dto/ResultDto.java
+++ ztbd-relational/src/main/java/pl/pk/ztbdrelational/dto/ResultDto.java
@@ -1,21 +1,3 @@
 package pl.pk.ztbdrelational.dto;
 
-public class ResultDto {
-
-  private final long time;
-
-  private final long numberOfRecords;
-
-  public ResultDto(long time, long numberOfRecords) {
-    this.time = time;
-    this.numberOfRecords = numberOfRecords;
-  }
-
-  public long getTime() {
-    return time;
-  }
-
-  public long getNumberOfRecords() {
-    return numberOfRecords;
-  }
-}
+public record ResultDto(long time, long numberOfRecords) {}
