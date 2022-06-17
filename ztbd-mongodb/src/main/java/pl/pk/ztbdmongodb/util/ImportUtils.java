7e72d0a764f43827d23c9b8e54810f511a93dba4
Dawid Aleksandrowicz <58815602+daleksandrowicz@users.noreply.github.com>
2022-06-17 11:00:50 +0200

    Update Docker

--- ztbd-mongodb/src/main/java/pl/pk/ztbdmongodb/util/ImportUtils.java
+++ ztbd-mongodb/src/main/java/pl/pk/ztbdmongodb/util/ImportUtils.java
@@ -3,9 +3,11 @@ package pl.pk.ztbdmongodb.util;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,9 +21,17 @@ public class ImportUtils {
         return Arrays.asList(split);
     }
 
+//    public static List<String> linesFromResource(String resource) throws IOException {
+//        Resource input = new ClassPathResource(resource);
+//        Path path = input.getFile().toPath();
+//        return Files.readAllLines(path);
+//    }
+
     public static List<String> linesFromResource(String resource) throws IOException {
         Resource input = new ClassPathResource(resource);
-        Path path = input.getFile().toPath();
-        return Files.readAllLines(path);
+        InputStream is = input.getInputStream();
+        InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(streamReader);
+        return reader.lines().toList();
     }
 }
